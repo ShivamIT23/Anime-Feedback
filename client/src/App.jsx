ca4d5f2035bcb55149a1a87049a6c1cc600d1a5d
ShivamIT23 <shivam.gupta.23112003@gmail.com>
2024-06-14 02:04:33 +0530

    Adding Code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,39 @@
 
 import AnimeBackground from '/AnimeBackground1.png'
 import backbox from '/back-box.png';
-import './App.css'
 import { NavBar } from './NavBar/NavBar'
-import { Heading } from './Heading';
+import { Heading } from './Components/Heading';
+// import { BrowserRouter , Route , Routes } from 'react-router-dom';
+import { useRef } from 'react';
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
+import { Button } from './Components/Button';
+import EastIcon from '@mui/icons-material/East';
+
 
 function App() {
+  const cursorCircle = useRef(null);
+  useGSAP(() => {
+    window.addEventListener("mousemove", function (dets) {
+      gsap.to(cursorCircle.current, {
+        duration:.8,
+        left: dets.x,
+        top: dets.y,
+      });
+    });
+  });
 
   return (
-      <div className='App'>
+    <>
+    <div ref={cursorCircle} id='crsr'></div>
+    <div className='App'>
         <NavBar />
         <Heading />
+        <Button> Get Started <sub><EastIcon/></sub></Button>
           <img src={AnimeBackground} className="logo" alt="Background-girl" />
           <img src={backbox} className='App-box' alt="Box-back" />
       </div>
+    </>
   )
 }
 
---
+++ client/src/Components/Button.jsx
@@ -0,0 +1,10 @@
+
+
+export const Button = ({children}) => {
+  return (
+    <div className="Common-button">
+      <button>{children}</button>
+    </div>
+    
+  )
+}
